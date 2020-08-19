a305e790f12948e7a95e44eaf686353fa30ca838
cgokce <cagri.gokce@boun.edu.tr>
2020-08-19 14:26:00 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Nav from './Nav';
+import SearchArea from './SearchArea';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <SearchArea />
+    </div>
+  );
+}
+
+export default App;
